9623df84378a811ae792c316401b7dc07f761d69
shibamghosh <145542344+SHIBAM-GHOSH@users.noreply.github.com>
2026-08-02 11:48:20 +0530

    Time: 0 ms (100%), Space: 9.6 MB (76.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+    bool isAnagram(string s, string t) 
+    {   
+        if(s.length() != t.length()) return false;
+        vector<int>freq(26,0); // vectorfor countin 26 Letters freq
 
-        if(s==t) return true;
-        return false;
+        //travers the string s
+        for(char ch : s)
+            {
+                freq[ch-'a']++;
+            }
         
+        //subratc freq from t
+        for(char k : t)
+            {
+                freq[k-'a']--;
+            }
+        
+        for(int i : freq)
+            {
+                if(i!=0) return false;
+            }
+
+        return true;
+
+
     }
 };
