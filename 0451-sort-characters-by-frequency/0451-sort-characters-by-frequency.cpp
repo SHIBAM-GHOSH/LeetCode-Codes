21be98e7b4c8823c1ec24d610cbc72ebc3609a0d
shibamghosh <145542344+SHIBAM-GHOSH@users.noreply.github.com>
2026-08-02 16:53:26 +0530

    Time: 0 ms (100%), Space: 10.8 MB (88.94%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string frequencySort(string s) 
+    {
+        int n = s.length();
+
+        map<char, int> mp1;
+        //coutn freq
+        for(char ch : s)
+            {
+                mp1[ch]++;
+            }
+        
+        //sort according ot freq
+        priority_queue<pair<int, char>> pq;
+
+        for(auto it :mp1)
+            {
+                pq.push({it.second, it.first});
+            }
+        
+        //xtarct trhe letetr and make string
+        string ans = "";
+        while(pq.empty() == false)
+            {
+                auto[c,letter] = pq.top(); pq.pop();
+
+                //append leter c times
+                //string w = "";
+                while(c--)
+                {
+                    ans+= letter;
+                }
+
+                //ans=ans+w;
+
+            }
+        return ans;
+
+
+    }
+};
