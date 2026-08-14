ff18adb87930ee3e83d49e0a47f0132cbcc71444
shibamghosh <145542344+SHIBAM-GHOSH@users.noreply.github.com>
2026-08-15 00:26:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (57.72%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -17,15 +17,13 @@ public:
                     j++;  //then movw forward 
                 } 
             //shrink, when more than 2 found
-            else if(mp[s[j]] == 2) // elent already exist 2 times. need to kick extra out
+            else //if(mp[s[j]] == 2) // elent already exist 2 times. need to kick extra out
                 {       
-                    while(mp[s[j]] == 2)//when we kcik one extra we can include the curretn same ele at j rigth now  
-                        {
-                            mp[s[i]]--;//we decremet
-                            i++; //shink the wind fromleft   
-                        }
+                    //when we kcik one extra we can include the curretn same ele at j rigth now  
+                    mp[s[i]]--;//we decremet
+                    i++; //shink the wind fromleft   
+                        
                 }
-            
             max_len = max(max_len,  j-i);
         }
 
