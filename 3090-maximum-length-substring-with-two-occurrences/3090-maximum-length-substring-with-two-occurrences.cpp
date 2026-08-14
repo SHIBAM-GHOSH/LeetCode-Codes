9cb9c7842f1745b0ffe72a4316b5c8236fad4c74
shibamghosh <145542344+SHIBAM-GHOSH@users.noreply.github.com>
2026-08-15 00:25:34 +0530

    Time: 0 ms (100%), Space: 9.7 MB (44.69%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -13,17 +13,17 @@ public:
             //expand
             if( mp[s[j]]<2)
                 {
-                    mp[s[j]]++; //inceremtn
-                    j++;
-                }
-            
+                    mp[s[j]]++; //inceremtn.add itto mapfirst
+                    j++;  //then movw forward 
+                } 
             //shrink, when more than 2 found
-            else if(mp[s[j]] == 2) //already 2 ele exist
-                {   
-                    
-                        mp[s[i]]--;
-                        i++; //shink the wind fromleft     
-                   
+            else if(mp[s[j]] == 2) // elent already exist 2 times. need to kick extra out
+                {       
+                    while(mp[s[j]] == 2)//when we kcik one extra we can include the curretn same ele at j rigth now  
+                        {
+                            mp[s[i]]--;//we decremet
+                            i++; //shink the wind fromleft   
+                        }
                 }
             
             max_len = max(max_len,  j-i);
