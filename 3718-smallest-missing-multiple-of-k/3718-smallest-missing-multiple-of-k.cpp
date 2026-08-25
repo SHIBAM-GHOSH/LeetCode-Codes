bbd9b0edd2784876e3df800adc92623d92088727
shibamghosh <145542344+SHIBAM-GHOSH@users.noreply.github.com>
2026-08-25 23:05:36 +0530

    Time: 0 ms (100%), Space: 24.1 MB (82.33%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) 
     {
-        unordered_set<int> s(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
 
+        int m = k; //m is the multiple of k, inceresae as we go forward
+        for(int i=0;i<nums.size();i++)
+            {
+                if(nums[i]%k == 0 and nums[i]>m) return m;
+                if(nums[i]%k == 0 and nums[i] == m) 
+                    {
+                        m=m+k;
+                    }
 
-        int m =k;
-        while( s.count(m)) // false when multiple fo k i.e is not thses in set
-        {
-            m=m+k;
-        }
+            }
 
         return m;
-
-
 
     }
 };
